43db3fa88e7d41a83ebe740ca16b48aa709fe949
ertbjerg <ertbjerg@gmail.com>
2021-09-19 09:26:12 +0200

    import mazes

--- 2dgridgraph.cpp
+++ 2dgridgraph.cpp
@@ -1,2 +1,86 @@
+#include <cmath>
 #include "2dgridgraph.hpp"
 
+GridGraph::GridGraph(const std::string& s)
+{
+    int x1=0, y2=0;
+    for (auto &c: s) {
+        if (c == '#') { //not part of the maze
+            x1++; 
+            continue;
+        } 
+        if (c == '\n') {
+            x1 = 0;
+            y2++;
+            continue;
+        }
+
+        this->nodes.push_back(std::tuple<int, int> {x1, y2});
+        x1++;
+
+        nodeid_t id = this->nodes.size() - 1; // id is index
+        if (c == 'S') {
+            this->startpoint = id;
+        } 
+        if (c == 'G') {
+            this->goalpoint = id;
+        }
+    }
+    // for (auto &p: this->nodes) {
+    //     std::cout << std::get<0>(p) << " " << std::get<1>(p) << std::endl;
+    // }
+    // auto &p = this->nodes[this->startpoint];
+    // std::cout << this->startpoint << " " << std::get<0>(p) << " " << std::get<1>(p) << std::endl;
+    // auto &q = this->nodes[this->goalpoint];
+    // std::cout << this->goalpoint << " " << std::get<0>(q) << " " << std::get<1>(q) << std::endl;
+}
+
+int GridGraph::manhattan_distance(nodeid_t to, nodeid_t from) 
+{
+    auto p = this->nodes[to];
+    auto q = this->nodes[from];
+    return std::abs(std::get<0>(p) - std::get<0>(q)) + std::abs(std::get<1>(p) - std::get<1>(q));
+}
+
+int GridGraph::start() {return startpoint;}
+int GridGraph::goal() {return goalpoint;}
+
+TEST(GridGraphClientInterface, testcase1) 
+{
+    std::string asciimap = 
+        "S.#...\n"
+        "..#.#.\n"
+        "..#.#.\n"
+        "....#G\n";
+    GridGraph gg(asciimap);
+    EXPECT_NE(gg.start(), gg.goal());
+}
+
+TEST(GridGraphImplementation, testcase1)
+{
+    std::string asciimap = 
+        "S.#...\n"
+        "..#.#.\n"
+        "..#.#.\n"
+        "....#G\n";
+    GridGraph gg(asciimap);
+    EXPECT_EQ(gg.nodes.size(), 18);
+    EXPECT_EQ(std::get<0>(gg.nodes[gg.startpoint]), 0);
+    EXPECT_EQ(std::get<1>(gg.nodes[gg.startpoint]), 0);
+    EXPECT_EQ(std::get<0>(gg.nodes[gg.goalpoint]), 5);
+    EXPECT_EQ(std::get<1>(gg.nodes[gg.goalpoint]), 3);
+}
+
+TEST(GridGraphImplementation, testcase2)
+{
+    std::string asciimap = 
+        "S.#...\n"
+        "..#.#.\n"
+        "..#.#.\n"
+        "....#G\n";
+    GridGraph gg(asciimap);
+    EXPECT_EQ(gg.manhattan_distance(gg.startpoint, gg.goalpoint), 8);
+    EXPECT_EQ(gg.manhattan_distance(gg.goalpoint, gg.startpoint), 8);
+    EXPECT_EQ(gg.manhattan_distance(0, 1), 1);
+    EXPECT_EQ(gg.manhattan_distance(2, 1), 2);
+}
--- 2dgridgraph.hpp
+++ 2dgridgraph.hpp
@@ -1,26 +1,43 @@
 #include <string>
 #include <tuple>
-#include <set>
+#include <vector>
+#include "gtest/gtest.h"
+
 
 #pragma once
 
-/* two points are connected iff manhattan distance 1 */
+// (0,0) -- 1.coordinate --->
+//   |
+// 2. coordinate
+//   |
+//   V
 
-struct GridPoint
-{
-public: 
-    int id;
-    std::tuple<int, int> p;
-};
-
+/* two points of a GridGraph are connected iff manhattan distance 1 */
 class GridGraph 
 {
-public:
+public:  // 
+    typedef int nodeid_t;
+
+public: // Client interface
     /* construct from string 
      * string s: an ascii map of a maze 
      */
-    GridGraph(std::string s);
+    GridGraph(const std::string& s);
+
+    nodeid_t start();
+    nodeid_t goal();
+
+public: // A*-interface
+    std::vector<nodeid_t> neighboors(nodeid_t p);
+    int cost(nodeid_t to, nodeid_t from) {return manhattan_distance(to, from);};
+    int eta(nodeid_t target, nodeid_t from) {return manhattan_distance(target, from);};
 
 private:
-    std::set<GridPoint> nodes;
+    std::vector<std::tuple<int, int>> nodes;
+    nodeid_t startpoint;
+    nodeid_t goalpoint;
+    int manhattan_distance(nodeid_t to, nodeid_t from);
+    
+    FRIEND_TEST(GridGraphImplementation, testcase1);
+    FRIEND_TEST(GridGraphImplementation, testcase2);
 };
